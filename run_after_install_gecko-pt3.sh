450e7c6cbd0ffe3c741927cd3e9807d0cf6c112c
Daniel Pedrotti <93792785+devadarta@users.noreply.github.com>
2023-02-17 11:47:46 -0300

    add zsh plugins

--- run_after_install_gecko-pt3.sh
+++ run_after_install_gecko-pt3.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#   Without sudo  #
+
 echo "[`date`] Install NeoVim Plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-#   Without sudo  #
 #echo "[`date`] Install LunarVim"
 #LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
 # Uninstall LunarVim
@@ -13,11 +14,13 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 #echo "[`date`] export Cargo Bin and local Bin - Used in LunarVim"
 #cat "export PATH=$HOME/.cargo/bin:$HOME/.local/bin:$PATH" >> $HOME/.zshrc 
 
-# PowerLevel10k ?
-#echo "[`date`] Install PowerLevel10k (pending)"
-# Recomended Fonts // files
-#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.config/powerlevel10k
-#echo 'source $HOME/.config/powerlevel10k/powerlevel10k.zsh-theme' >> $HOME/.zshrc
+echo "[`date`] Install PowerLevel10k plugin"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.config/zsh/plugins/powerlevel10k
+#echo 'source $HOME/.config/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+echo "[`date`] Install ash-autosuggestions plugin"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+#echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >>~/.zshrc
 
 #echo "[`date`] Install asdf"
 #git clone https://github.com/asdf-vm/asdf.git $HOME/.config/asdf --branch v0.10.2
